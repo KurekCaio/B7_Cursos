c99bbcf1d2aaf1c82b2f253fbfbb9d51e9c3e931
KurekCaio <caiokurek@hotmail.com>
2022-11-08 20:01:45 -0300

    Até aula 05

---
+++ Curso JS/script.js
@@ -0,0 +1,7 @@
+function clicou() {
+    const teste = document.querySelector('#teste');
+    console.log(teste.children);
+}
+
+// let botaozera = document.querySelector('.botao');
+// botaozera.addEventListener("click", () => {clicou()});
