6242385363e7469424241fa6aa666c770e034c65
KurekCaio <caiokurek@hotmail.com>
2022-12-14 16:01:59 -0300

    Apagando pós botão e salvando info do  anterior

--- Curso React/prototipo-cafe/src/App.tsx
+++ Curso React/prototipo-cafe/src/App.tsx
@@ -2,18 +2,25 @@ import {useState} from 'react'
 import styles from './App.module.css';
 import logoMaua from './assets/logo-IMT.png'
 import logoNspi from './assets/nspi-logo.png'
+import { Pedido, processRequest } from './helper/processRequest';
 
 const App = () => {
 
 const [name, setName] = useState("")
 const [phone, setPhone] = useState(0)
-const [coffe, setCoffe] = useState(0)
+const [coffee, setCoffee] = useState(0)
+const [show, setShow] = useState('')
+// const [show,setShow] = useState<Pedido | null>(null)
 
 const sendRequest = () => {
-  if (name && phone) {
-    alert('a')
+  if (name && phone && coffee) {
+    setShow(processRequest(name,phone,coffee))
+
+    setName("")
+    setPhone(0)
+    setCoffee(0)
   } else {
-    alert({coffe})
+    alert("Preencha todos os campos")
   }
 }
 
@@ -31,7 +38,7 @@ const sendRequest = () => {
 
       <div className={styles.titleContainer}> 
         <h1>CAS</h1>
-        <h2>Coffe Advanced Service</h2>
+        <h2>Coffee Advanced Service</h2>
       </div>
 
       <div className={styles.container}>
@@ -40,21 +47,26 @@ const sendRequest = () => {
 
           <input type="string"
           placeholder="Digite seu nome"
-          defaultValue={''}
+          value={name.length > 0 ? name : ''}
           onChange={e => setName(e.target.value)}
           />
 
           <input type="number"
           placeholder="Digite seu n° de celular"
-          defaultValue={''}
+          value={phone > 0 ? phone : ''}
           onChange={e => setPhone(parseInt(e.target.value))}
           />
 
-          <select name="t1" onChange={e => setCoffe(parseInt(e.target.value))}>
-            <option value="-1" disabled selected>--Escolha o sabor do café--</option>
-            <option value="0">Café puro</option>
-            <option value="1">Capuccino</option>
-            <option value="2">Creme brullê</option>
+          <select 
+          id='saborCafe' 
+          onChange={e => setCoffee(parseInt(e.target.value))} 
+          defaultValue="0"
+          value={coffee !== 0 ? coffee : 0}
+          >
+            <option value="0" disabled>--Escolha o sabor do café--</option>
+            <option value="1">Café puro</option>
+            <option value="2">Capuccino</option>
+            <option value="3">Creme brullê</option>
           </select>
 
           <label>
@@ -65,7 +77,13 @@ const sendRequest = () => {
           <button onClick={sendRequest}>Realizar pedido</button>
         </div>
 
-        <div className={styles.rightSide}>blob</div>
+        <div className={styles.rightSide}>
+          <p>Nome: {name}. Telefone: {phone}. Café: {coffee-1}.</p>
+          <ul>
+            <li>{name} / {phone} / {coffee} </li>
+            <li>{show}</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
