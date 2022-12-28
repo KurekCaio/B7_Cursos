762f96dab92a11980db7198180ac732edd86090e
KurekCaio <caiokurek@hotmail.com>
2022-12-28 17:25:49 -0300

    .

--- Curso React/react-primeiro/src/App.tsx
+++ Curso React/react-primeiro/src/App.tsx
@@ -1,13 +1,11 @@
-import * as C from './AppStyles'
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+
   return (
-    <C.Container bgColor="#CCC">
-         Texto do Container.
-         <a className="link" href="">Linkzada</a>
-         <C.Botao bg="#FF0000" >Clique maior</C.Botao>
-         <C.Botao bg="#00FF00" small>Clique menor</C.Botao>
-    </C.Container>
+    <div>
+      ...
+    </div>
   );   
 }
 export default App;
